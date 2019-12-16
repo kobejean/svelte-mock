6ba18c0e1ebec340ca3f1bf8e84c4dba8e02d1c9
Jean Flaherty <jean.flaherty@ihsmarkit.com>
2019-12-16 11:02:38 -0700

    Support for Svelte 3.15.0

--- src/inspect/get-props.js
+++ src/inspect/get-props.js
@@ -1,11 +1,25 @@
+/* eslint-disable camelcase */
 import { mapValues, get } from 'lodash';
 import { isSvelteVersion } from '@utils/version';
 
 const getPropsV2 = (component) => component.get();
-const getPropsV3 = (component) => {
+const getPropsV3_15_0 = (component) => {
+  const propKeys = get(component, ['$$', 'props'], {});
+  const ctx = get(component, ['$$', 'ctx'], []);
+  return mapValues(propKeys, (key) => ctx[key]);
+}; ;
+const getPropsLatest = (component) => {
   const propIndices = get(component, ['$$', 'props'], {});
   const ctx = get(component, ['$$', 'ctx'], []);
   return mapValues(propIndices, (index) => ctx[index]);
 };
-export const getProps = isSvelteVersion('3.0.0', '<') ?
-  getPropsV2 : getPropsV3;
+
+
+export const getProps = (() => {
+  if (isSvelteVersion('3.0.0', '<')) {
+    return getPropsV2;
+  } else if (isSvelteVersion('3.16.0', '<')) {
+    return getPropsV3_15_0;
+  }
+  return getPropsLatest;
+})();
---
+++ src/mocks/MockComponent.js
@@ -0,0 +1,15 @@
+/* eslint-disable camelcase */
+import { isSvelteVersion } from '@utils/version';
+import { makeComponent } from '@utils/compile';
+import { resolveDefault } from '@utils/import';
+
+
+export default (() => {
+  if (isSvelteVersion('3.0.0', '<')) {
+    return makeComponent('<slot></slot>', 'MockComponent');
+  } else if (isSvelteVersion('3.16.0', '<')) {
+    return resolveDefault(require('./MockComponentV3_15_0'));
+  } else {
+    return resolveDefault(require('./MockComponentLatest'));
+  }
+})();
