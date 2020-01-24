07a7b2160ae00c77a1048f90ae58d695ff0ee56e
Jean Flaherty <kobejean@me.com>
2020-01-24 15:03:33 -0700

    dynamic instantiation tests for event handler expectations

--- test/fixtures/v3/EventHandlers.svelte
+++ test/fixtures/v3/EventHandlers.svelte
@@ -4,6 +4,8 @@
   export const secondClick = () => message = 'Click Event Second'
   export const thirdCustom = () => message = 'Custom Event Third'
   export const thirdRandom = () => message = 'Random Event Third'
+  export const fourthRandom = () => message = 'Focus Event Fourth'
+  export let component = Paragraph;
 </script>
 
 <div>
@@ -11,4 +13,5 @@
     <Paragraph text="First" />
     <Paragraph text="Second" on:click={secondClick} />
     <Paragraph text="Third"  on:custom={thirdCustom} on:random={thirdRandom} />
+    <svelte:component this="{ component }" text="Fourth" on:focus={fourthRandom} />
 </div>
